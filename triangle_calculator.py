6b6cde25afe8b75bac6c67a96634fe5063668b12
Gabriel-Note <gabriel.emil.note@gmail.com>
2025-03-31 14:51:45 +0200

    Final commit

--- triangle_calculator.py
+++ triangle_calculator.py
@@ -1,54 +1,28 @@
-
-# opening and reading the textfiles
-# Using 3 different ones for testing purposes
 file1 = open("triangle.txt", "r")
-file2 = open("triangle0.txt", "r")
-file3 = open("triangle1.txt", "r")
-
 triangle = file1.readlines()
-triangle0 = file2.readlines()
-triangle1 = file3.readlines()
 
 
-
-# smallList = [
-#         3,
-#        7,4,
-#       2,4,6,
-#      8,5,9,3,
-#     5,6,2,9,4
-# ]
-# smallList2 = [
-#     [3],
-#     [7,4],
-#     [2,4,6],
-#     [8,5,9,3],
-#     [5,6,2,9,4]
-# ]
-
+#Global variables
 triList = []
 
-# Creating a nested list and converting to Int using
-for line in triangle0:
+for line in triangle:
     StringList = line.split()
     IntList = list(map(int, StringList))
     triList.append(IntList)
 
-# for line in triangle:
-#     StringList = line.split()
-#     IntList = list(map(int, StringList))
-#     triList.append(IntList)
+# Using dynamic programming I want to go through the list starting from the bottom row and work my way up,
+# while adding the new number to the row above so that I in the end finally have one final number at the top of the list/triangle.
 
+rowIndex = len(triList) - 2
+while rowIndex >= 0:
+    colIndex = 0
+    while colIndex <= rowIndex:
+        currentNumber = triList[rowIndex][colIndex]
+        leftAdjacent = triList[rowIndex+1][colIndex]
+        rightAdjacent = triList[rowIndex+1][colIndex+1]
+        currentNumber = max(currentNumber+leftAdjacent,currentNumber+rightAdjacent)
+        triList[rowIndex][colIndex] = currentNumber
+        colIndex += 1
+    rowIndex -= 1
 
-print("new line")
-print(triList)
-print(triList[2][2])
-print(triList[2][2] + triList[3][3])
-
-
-
-
-
-
-#print(triangle0)
-#print(smallList2[1][1])
+print("Result: " + str(triList[0][0]))
